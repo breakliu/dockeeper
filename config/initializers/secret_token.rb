60f93ffbe8ad749224ef3ae5dfe00aab6f9dc224
slackcode at home <slackcode@gmail.com>
2011-10-18 00:39:33 +0800

    first commit, http auth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dockeeper::Application.config.secret_token = '6537a3c7959f1be31dbfd8c4a1a63789234406b705bfdd29acd2d1d00fa03e2392e23cf7892663a6999c38acdbbafaa8d31d4cdcb27fa04315c5b88c9dcc5845'
